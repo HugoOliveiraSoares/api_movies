9e88759372e1ce763a6bcddba0ff308ebbaf2b92
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2024-05-21 18:01:41 -0300

    justification of why to use record

--- src/main/java/br/com/film_catalog/Movie.java
+++ src/main/java/br/com/film_catalog/Movie.java
@@ -1,3 +1,4 @@
 package br.com.film_catalog;
 
+//TIP O uso de um record é valido neste cenário, pois a Classe Movie pode ser usado como DTO
 public record Movie(String title, String imageUrl, float rating, int year){}
